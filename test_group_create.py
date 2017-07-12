11858347d92b979d81f3a524c213d3cecce9b875
priestsergey <ter77@ukr.net>
2017-07-12 20:29:21 +0300

    Extracted method

--- test_group_create.py
+++ test_group_create.py
@@ -11,7 +11,7 @@ def is_alert_present(wd):
 class test_group_create(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(60)
+        self.wd.implicitly_wait(15)
     
     def test_test_group_create(self):
         wd = self.wd
@@ -25,16 +25,15 @@ class test_group_create(unittest.TestCase):
         # Creat
         wd = self.wd
         wd.find_element_by_name("new").click()
-        wd.find_element_by_name(group_name).click()
-        wd.find_element_by_name(group_name).clear()
-        wd.find_element_by_name(group_name).send_keys(group_name)
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group_name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
         wd.find_element_by_name("group_header").send_keys(group_header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group_footer)
-        wd.find_element_by_name(group_name).click()
         wd.find_element_by_name("submit").click()
 
     def open_group_page(self):
